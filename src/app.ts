3ffe5df3a1a6deebc5aa4934d5ad9ce12b633463
Ubong Edem <ubongedem78@gmail.com>
2023-12-23 03:54:22 +0100

    setup

---
+++ src/app.ts
@@ -0,0 +1,69 @@
+import * as fs from "fs";
+const md = require("markdown-it")({
+  html: true, // Enable HTML tags in source
+  breaks: true, // Convert '\n' in paragraphs into <br>
+  linkify: true, // Autoconvert URL-like text to links
+});
+import { fetchGitHubData } from "./fetchGitHubData";
+const githubUsername = "Ubongedem78";
+
+const ossProjectRepos = ["ShuttleQ", "xyz"];
+
+const websiteUrl = "https://wr78.me";
+const twitterUrl = "https://twitter.com/usernameissnull";
+const linkedinUrl = "https://linkedin.com/in/ubongedem78/";
+
+async function generateMarkdown() {
+  const websiteBadge = `[![Website Badge](https://img.shields.io/badge/-Website-3B7EBF?style=for-the-badge&logo=amp&logoColor=white)](${websiteUrl})`;
+  const linkedinBadge = `[![Linkedin Badge](https://img.shields.io/badge/-LinkedIn-3B7EBF?style=for-the-badge&logo=Linkedin&logoColor=white)](${linkedinUrl})`;
+  const twitterBadge = `[![Twitter Badge](https://img.shields.io/badge/-@usernameissnull-3B7EBF?style=for-the-badge&logo=x&logoColor=white)](${twitterUrl})`;
+  const profileCountBadge = `![Profile Views Count Badge](https://komarev.com/ghpvc/?username=${githubUsername}&style=for-the-badge)`;
+
+  const githubStatsCardDark = `[![GitHub-Stats-Card-Dark](https://github-readme-stats.vercel.app/api?username=${githubUsername}&show_icons=true&hide_border=true&include_all_commits=true&card_width=600&custom_title=GitHub%20Open%20Source%20Stats&title_color=3B7EBF&text_color=FFF&icon_color=3B7EBF&hide=contribs&show=reviews,prs_merged,prs_merged_percentage&theme=transparent#gh-dark-mode-only)](https://github.com/${githubUsername}/${githubUsername}#gh-dark-mode-only)`;
+  const githubStatsCardLight = `[![GitHub-Stats-Card-Light](https://github-readme-stats.vercel.app/api?username=${githubUsername}&show_icons=true&hide_border=true&include_all_commits=true&card_width=600&custom_title=GitHub%20Open%20Source%20Stats&title_color=3B7EBF&text_color=474A4E&icon_color=3B7EBF&hide=contribs&show=reviews,prs_merged,prs_merged_percentage&theme=transparent#gh-light-mode-only)](https://github.com/${githubUsername}/${githubUsername}#gh-light-mode-only)`;
+
+  const markdownText = `<div align="center">\n
+
+  ${websiteBadge} ${linkedinBadge} ${twitterBadge} ${profileCountBadge}\n
+
+  ---\n
+
+  👋🏾Dedicated innovator with a fervor for conceptualizing and bringing to fruition cutting-edge products designed to streamline processes, automate tasks, and elevate overall quality of life. I am focused on transforming ideas into impactful, user-centric experiences.\n
+
+  ---\n
+
+  ${githubStatsCardDark}\n
+  ${githubStatsCardLight}\n
+
+  </div>\n
+
+  ---\n
+
+  ## Highlights
+
+  <details>\n
+  <summary>Projects</summary>\n
+  <br />
+  Here are some of my other projects you might want to check out that are not pinned:\n
+  <br />\n<br />
+  ${await fetchGitHubData(ossProjectRepos)}\n
+  </details>\n
+
+  <details>\n
+  <summary>Quick Tips</summary>\n\n
+  - 💬 How to reach me: DM [@usernameissnull](https://twitter.com/usernameissnull) on X (Twitter).\n
+  - 📬 Where to find me: Send a game request on [chess.com](https://lichess.org/@/wrapid78).\n
+  - 📖 Book recommendations: [Knowing God by J. I. Packer](https://bit.ly/3EdCFUW) and [Atomic Habits by James Clear](https://bit.ly/45r1kBH).\n
+  </details>\n
+  ---\n
+`;
+
+  const result = md.render(markdownText);
+
+  fs.writeFile("README.md", result, (error) => {
+    if (error) throw new Error(`Something went wrong: ${error}.`);
+    console.log(`✅ README.md file was succesfully generated.`);
+  });
+}
+
+generateMarkdown();
