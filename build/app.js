da56aea58f9ef98d1be207e2cf12b7654a965d37
Ubong Edem <ubongedem78@gmail.com>
2023-12-27 23:27:07 +0100

    Fix:resize

--- build/app.js
+++ build/app.js
@@ -59,15 +59,15 @@ function generateMarkdown() {
     <img
       src="https://raw.githubusercontent.com/devicons/devicon/master/icons/nodejs/nodejs-original-wordmark.svg"
       alt="nodejs"
-      width="40"
-      height="40"
+      width="60"
+      height="60"
     /> </a
   ><a href="https://expressjs.com" target="_blank" rel="noreferrer">
     <img
       src="https://raw.githubusercontent.com/devicons/devicon/master/icons/express/express-original-wordmark.svg"
       alt="express"
-      width="40"
-      height="40"
+      width="60"
+      height="60"
     /> </a
   ><a
     href="https://developer.mozilla.org/en-US/docs/Web/JavaScript"
@@ -77,77 +77,77 @@ function generateMarkdown() {
     <img
       src="https://raw.githubusercontent.com/devicons/devicon/master/icons/javascript/javascript-original.svg"
       alt="javascript"
-      width="40"
-      height="40"
+      width="60"
+      height="60"
   /></a>
   <a href="https://git-scm.com/" target="_blank" rel="noreferrer">
     <img
       src="https://www.vectorlogo.zone/logos/git-scm/git-scm-icon.svg"
       alt="git"
-      width="40"
-      height="40"
+      width="60"
+      height="60"
     /> </a
   ><a href="https://www.w3.org/html/" target="_blank" rel="noreferrer">
     <img
       src="https://raw.githubusercontent.com/devicons/devicon/master/icons/html5/html5-original-wordmark.svg"
       alt="html5"
-      width="40"
-      height="40"
+      width="60"
+      height="60"
     /> </a
   ><a href="https://jestjs.io" target="_blank" rel="noreferrer">
     <img
       src="https://www.vectorlogo.zone/logos/jestjsio/jestjsio-icon.svg"
       alt="jest"
-      width="40"
-      height="40"
+      width="60"
+      height="60"
     /> </a
   ><a href="https://www.mongodb.com/" target="_blank" rel="noreferrer">
     <img
       src="https://raw.githubusercontent.com/devicons/devicon/master/icons/mongodb/mongodb-original-wordmark.svg"
       alt="mongodb"
-      width="40"
-      height="40"
+      width="60"
+      height="60"
   /></a>
   <a href="https://www.typescriptlang.org/" target="_blank" rel="noreferrer">
     <img
       src="https://raw.githubusercontent.com/devicons/devicon/master/icons/typescript/typescript-original.svg"
       alt="typescript"
-      width="40"
-      height="40"
+      width="60"
+      height="60"
     /> </a
   ><a href="https://www.arduino.cc/" target="_blank" rel="noreferrer"
     ><img
       src="https://raw.githubusercontent.com/devicons/devicon/master/icons/arduino/arduino-original-wordmark.svg"
       alt="arduino"
-      width="40"
-      height="40"
+      width="60"
+      height="60"
     /> </a
   ><a href="https://aws.amazon.com/" target="_blank" rel="noreferrer"
     ><img
       src="https://raw.githubusercontent.com/devicons/devicon/master/icons/amazonwebservices/amazonwebservices-original.svg"
       alt="aws"
-      width="40"
-      height="40"
+      width="60"
+      height="60"
     /> </a
   ><a href="https://www.docker.com/" target="_blank" rel="noreferrer"
     ><img
       src="https://raw.githubusercontent.com/devicons/devicon/master/icons/docker/docker-original.svg"
       alt="docker"
-      width="40"
-      height="40" /></a
+      width="60"
+      height="60" /></a
   ><a href="https://www.rabbitmq.com/" target="_blank" rel="noreferrer"
     ><img
       src="https://www.rabbitmq.com/img/logo-rabbitmq.svg"
       alt="rabbitmq"
-      width="80"
-      height="40"
+      width="60"
+      height="60"
   /></a>
   <a href="https://www.postgresql.org/" target="_blank" rel="noreferrer">
     <img
       src="https://raw.githubusercontent.com/devicons/devicon/master/icons/postgresql/postgresql-original.svg"
       alt="postgresql"
-      width="40"
-      height="40"
+      width="60"
+      height="60"
     />
   </a>
   <a
@@ -158,31 +158,31 @@ function generateMarkdown() {
     <img
       src="https://raw.githubusercontent.com/devicons/devicon/master/icons/github/github-original.svg"
       alt="github-actions"
-      width="40"
-      height="40"
+      width="60"
+      height="60"
     />
   </a>
   <a href="https://typeorm.io/" target="_blank" rel="noreferrer">
     <img
       src="https://raw.githubusercontent.com/typeorm/typeorm/master/resources/logo_big.png"
       alt="typeorm"
-      width="80"
-      height="40"
+      width="60"
+      height="60"
     />
   </a>
   <a href="https://graphql.org" target="_blank" rel="noreferrer">
     <img
       src="https://www.vectorlogo.zone/logos/graphql/graphql-icon.svg"
       alt="graphql"
-      width="40"
-      height="40"
+      width="60"
+      height="60"
   /></a>
   <a href="https://golang.org/" target="_blank" rel="noreferrer">
     <img
       src="https://raw.githubusercontent.com/devicons/devicon/master/icons/go/go-original.svg"
       alt="golang"
-      width="40"
-      height="40"
+      width="60"
+      height="60"
     />
   </a>
   `;
